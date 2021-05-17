a7906637ec08242ecb15c8a5ccbc8f8b400d4969
Deren B <dbozerprogramming@gmail.com>
2021-05-17 15:42:32 -0400

    Get Element
    Src: Udemy Web Dev

---
+++ DocumentObjectModel.js
@@ -0,0 +1,16 @@
+//! DOM: Document Object Model
+//* Document Object contains all of the html
+
+
+//! GET ELEMENT BY ID
+//* Looks for an element in the HTML with a certain ID
+document.getElementById("ID");
+
+//! GET ELEMENT BY TAG NAME
+//* Tag name is not case sensitive
+document.getElementsByTagName('h1');
+
+//! GET ELEMENT BY CLASS NAME
+document.getElementsByClassName("");
+
+
