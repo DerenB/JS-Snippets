d5a95f48a30595a5819ea841983740cff60207ce
Deren B <47374311+DerenB@users.noreply.github.com>
2021-05-17 23:01:33 -0400

    Query Selector
    Src: Udemy Web Dev

--- DocumentObjectModel.js
+++ DocumentObjectModel.js
@@ -13,4 +13,38 @@ document.getElementsByTagName('h1');
 //! GET ELEMENT BY CLASS NAME
 document.getElementsByClassName("");
 
+//! INNER HTML
+//* Retrieves everything including tag names
+//* Can be used to change html
+document.querySelector('p').innerHTML;
+
+//! QUERY SELECTOR
+//* Can selection any CSS item by any style
+//* Element name, ID, etc
+//* Returns the first match when multiple elements of same type
+document.querySelector('h1');
+document.querySelector('.class');
+document.querySelector('#Id');
+document.querySelector('a[title="Java"]');
+
+//! QUESY SELECTOR, ALL
+//* Returns all of type instead of just first
+document.querySelectorAll('p');
+document.querySelectorAll('p a');
+
+//! TEXT, CONTENT
+//* Retrieves the text from the markup
+//* Retrieves everything including hidden elements
+document.querySelector('p').textContent;
+
+//! TEXT, INNER
+//* Retrieves the inner text of an element
+//* Will not retrieve hidden elements
+document.querySelector('p').innerText;
+
+
+
+
+
+
 
