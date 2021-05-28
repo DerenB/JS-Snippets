b334aa46a7040b43502bd64aa84bd26a72224fcd
Deren B <47374311+DerenB@users.noreply.github.com>
2021-05-28 14:15:16 -0400

    TypeOf

--- Notes.js
+++ Notes.js
@@ -521,6 +521,21 @@ try {
     console.log("Error");
 }
 
+//! TYPEOFF
+//* Returns a string indicating the type of the unevaluated operand
+console.log(typeof 42);
+// expected output: "number"
+
+console.log(typeof 'blubber');
+// expected output: "string"
+
+console.log(typeof true);
+// expected output: "boolean"
+
+console.log(typeof undeclaredVariable);
+// expected output: "undefined"
+
+
 
 
 
