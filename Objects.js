e468b4b7e1510dab9128f8f7d7f5b2877b8e8d3c
Deren B <47374311+DerenB@users.noreply.github.com>
2021-05-31 13:06:45 -0400

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -1,10 +1,44 @@
-
 //! TEST OBJECT
 const testObject = {
-    firstName: "Liara",
-    lastName: "T'Soni",
-    race: "Asari",
-    age: 106,
-    home: "Thessia",
-    recruitable: true
+	firstName: "Liara",
+	lastName: "T'Soni",
+	race: "Asari",
+	age: 106,
+	home: "Thessia",
+	recruitable: true,
+};
+
+//! CONSTRUCTOR FUNCTION
+function Dog() {
+	this.name = "Bob";
+	this.color = "brown";
+	this.numLegs = 4;
 }
+
+//! CONSTRUCT OBJECT
+let constructObj = new Dog();
+
+//! METHOD
+let dog = {
+	name: "Spot",
+	numLegs: 4,
+	sayLegs: function () {
+		return "This dog has " + dog.numLegs + " legs.";
+	},
+};
+
+//! PARAMETERS
+function Dog(name, color) {
+	this.name = name;
+	this.color = color;
+	this.numLegs = 4;
+}
+
+//! THIS
+let dog = {
+	name: "Spot",
+	numLegs: 4,
+	sayLegs: function () {
+		return "This dog has " + this.numLegs + " legs.";
+	},
+};
