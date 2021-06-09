9c21beef33d4ae07ab19d7bb8c1de9dd3f644287
Deren B <dbozerprogramming@gmail.com>
2021-06-08 23:13:40 -0400

    Format Seconds

--- Snippets.js
+++ Snippets.js
@@ -87,6 +87,21 @@ function factorialize(num) {
 	}
 }
 
+//! FORMAT SECONDS
+const formatSeconds = (s) => {
+	const [hour, minute, second, sign] =
+		s > 0
+			? [s / 3600, (s / 60) % 60, s % 60, ""]
+			: [-s / 3600, (-s / 60) % 60, -s % 60, "-"];
+
+	return (
+		sign +
+		[hour, minute, second]
+			.map((v) => `${Math.floor(v)}`.padStart(2, "0"))
+			.join(":")
+	);
+};
+
 //! INSERT ARRAY VALUES INTO ANOTHER ARRAY
 //* Uses splice to insert array values into another array at given index
 //* Does not modify the original two arrays
