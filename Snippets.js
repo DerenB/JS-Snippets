5019409084b1ab58651c8b613061e65a6fd1d74b
Deren B <dbozerprogramming@gmail.com>
2021-06-10 22:25:34 -0400

    Heap Sort

--- Snippets.js
+++ Snippets.js
@@ -102,6 +102,32 @@ const formatSeconds = (s) => {
 	);
 };
 
+//! HEAPSORT
+const heapsort = (arr) => {
+	const a = [...arr];
+	let l = a.length;
+
+	const heapify = (a, i) => {
+		const left = 2 * i + 1;
+		const right = 2 * i + 2;
+		let max = i;
+		if (left < l && a[left] > a[max]) max = left;
+		if (right < l && a[right] > a[max]) max = right;
+		if (max !== i) {
+			[a[max], a[i]] = [a[i], a[max]];
+			heapify(a, max);
+		}
+	};
+
+	for (let i = Math.floor(l / 2); i >= 0; i -= 1) heapify(a, i);
+	for (i = a.length - 1; i > 0; i--) {
+		[a[0], a[i]] = [a[i], a[0]];
+		l--;
+		heapify(a, 0);
+	}
+	return a;
+};
+
 //! INSERT ARRAY VALUES INTO ANOTHER ARRAY
 //* Uses splice to insert array values into another array at given index
 //* Does not modify the original two arrays
