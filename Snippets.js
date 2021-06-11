ac2a0e38179f273b181c4ab3a0d9166aaf3a79cb
Deren B <dbozerprogramming@gmail.com>
2021-06-10 22:25:10 -0400

    Quicksort

--- Snippets.js
+++ Snippets.js
@@ -198,6 +198,26 @@ function getIndexToIns(arr, num) {
 	return position;
 }
 
+//! QUICKSORT
+const quickSort = (arr) => {
+	const a = [...arr];
+	if (a.length < 2) return a;
+	const pivotIndex = Math.floor(arr.length / 2);
+	const pivot = a[pivotIndex];
+	const [lo, hi] = a.reduce(
+		(acc, val, i) => {
+			if (val < pivot || (val === pivot && i != pivotIndex)) {
+				acc[0].push(val);
+			} else if (val > pivot) {
+				acc[1].push(val);
+			}
+			return acc;
+		},
+		[[], []]
+	);
+	return [...quickSort(lo), pivot, ...quickSort(hi)];
+};
+
 //! REPEAT A STRING X TIMES
 //* Repeats a string input number of times
 function repeatStringNumTimes(str, num) {
