1693090e789fe26bc896df06b988db3c7a0119c9
Deren B <47374311+DerenB@users.noreply.github.com>
2021-06-11 23:37:02 -0400

    Bubble Sort

--- Snippets.js
+++ Snippets.js
@@ -37,6 +37,23 @@ function bouncer(arr) {
 	return myArr;
 }
 
+//! BUBBLE SORT
+const bubbleSort = (arr) => {
+	let swapped = false;
+	const a = [...arr];
+	for (let i = 1; i < a.length - 1; i++) {
+		swapped = false;
+		for (let j = 0; j < a.length - i; j++) {
+			if (a[j + 1] < a[j]) {
+				[a[j], a[j + 1]] = [a[j + 1], a[j]];
+				swapped = true;
+			}
+		}
+		if (!swapped) return a;
+	}
+	return a;
+};
+
 //! CAPITALIZE THE FIRST LETTER OF WORD
 //* Capitalizes the first letter of a string
 function capitalize(line) {
