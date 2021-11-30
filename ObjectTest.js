bc93e45b185c305dd5da010b2b931f03e6309d25
Deren B <dbozerprogramming@gmail.com>
2021-11-29 19:01:57 -0500

    Adding fish data

--- ObjectTest.js
+++ ObjectTest.js
@@ -23,6 +23,7 @@ let fishList = [];
 let bitterling = new Fish("Bitterling","bitterling.png",900,"River",1,0,2359,-1,-1,11,03,-1,-1);
 let paleChub = new Fish("Pale Chub","palechub.png",200,"River",1,0900,1600,-1,-1,1,12,-1,-1);
 let crucianCarp = new Fish("Crucian Carp","cruciancarp.png",160,"River",2,0,2359,-1,-1,1,12,-1,-1);
+let dace = new Fish("Dace","dace.png",240,"River",3,1600,900,-1,-1,1,12,-1,-1);
 
 fishList.push(bitterling);
 fishList.push(paleChub);
