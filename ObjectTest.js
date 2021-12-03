a4c1cdd54c94fedb2765c49019859f708a00a0ba
Deren B <dbozerprogramming@gmail.com>
2021-12-02 20:38:09 -0500

    Update ObjectTest.js

--- ObjectTest.js
+++ ObjectTest.js
@@ -26,6 +26,7 @@ let crucianCarp = new Fish("Crucian Carp","cruciancarp.png",160,"River",2,0,2359
 let dace = new Fish("Dace","dace.png",240,"River",3,1600,900,-1,-1,1,12,-1,-1);
 let carp = new Fish("Carp,","carp.png",300,"Pond",4,0,2359,-1,-1,1,12,-1,-1);
 let koi = new Fish("Koi","koi.png",4000,"Pond",4,1600,900,-1,-1,1,12,-1,-1);
+let goldfish = new Fish("Goldfish","goldfish.png",1300,"Pond",1,0,2359,-1,-1,1,12,-1,-1);
 
 fishList.push(bitterling);
 fishList.push(paleChub);
