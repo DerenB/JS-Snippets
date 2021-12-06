c43b1bcd8f9e7d74471c83defcc5a9362cb96894
Deren B <dbozerprogramming@gmail.com>
2021-12-06 01:03:21 -0500

    Changing Constructors

--- ObjectTest.js
+++ ObjectTest.js
@@ -1,38 +1,67 @@
 
 //Default constructor
 class Fish {
-    constructor(name,image,price,location,shadowSize,timeStartOne,timeEndOne,timeStartTwo,timeEndTwo,
-        monthStartOne,monthEndOne,monthStartTwo,monthEndTwo) {
+    constructor(name,image,price,location,shadowSize,timeStart,timeEnd,monthStart,monthEnd) {
         this.name = name;
         this.image = image;
         this.price = price;
         this.location = location;
         this.shadowSize = shadowSize;
-        this.timeStartOne = timeStartOne;
-        this.timeEndOne = timeEndOne;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+        this.monthStart = monthStart;
+        this.monthEnd = monthEnd;
+    }
+
+    constructor(name,image,price,location,shadowSize,timeStart,timeEnd,monthStart,monthEnd,
+        timeStartTwo,timeEndTwo,monthStartTwo,monthEndTwo) {
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.location = location;
+        this.shadowSize = shadowSize;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+        this.monthStart = monthStart;
+        this.monthEnd = monthEnd;
         this.timeStartTwo = timeStartTwo;
         this.timeEndTwo = timeEndTwo;
-        this.monthStartOne = monthStartOne;
-        this.monthEndOne = monthEndOne;
         this.monthStartTwo = monthStartTwo;
         this.monthEndTwo = monthEndTwo;
     }
+
+    // constructor(name,image,price,location,shadowSize,timeStartOne,timeEndOne,timeStartTwo,timeEndTwo,
+    //     monthStartOne,monthEndOne,monthStartTwo,monthEndTwo) {
+    //     this.name = name;
+    //     this.image = image;
+    //     this.price = price;
+    //     this.location = location;
+    //     this.shadowSize = shadowSize;
+    //     this.timeStartOne = timeStartOne;
+    //     this.timeEndOne = timeEndOne;
+    //     this.timeStartTwo = timeStartTwo;
+    //     this.timeEndTwo = timeEndTwo;
+    //     this.monthStartOne = monthStartOne;
+    //     this.monthEndOne = monthEndOne;
+    //     this.monthStartTwo = monthStartTwo;
+    //     this.monthEndTwo = monthEndTwo;
+    // }
 }
 
 let fishList = [];
 
 // Creating the fish array items
-let bitterling = new Fish("Bitterling","bitterling.png",900,"River",1,0,2359,-1,-1,11,03,-1,-1);
-let paleChub = new Fish("Pale Chub","palechub.png",200,"River",1,0900,1600,-1,-1,1,12,-1,-1);
-let crucianCarp = new Fish("Crucian Carp","cruciancarp.png",160,"River",2,0,2359,-1,-1,1,12,-1,-1);
-let dace = new Fish("Dace","dace.png",240,"River",3,1600,900,-1,-1,1,12,-1,-1);
-let carp = new Fish("Carp,","carp.png",300,"Pond",4,0,2359,-1,-1,1,12,-1,-1);
-let koi = new Fish("Koi","koi.png",4000,"Pond",4,1600,900,-1,-1,1,12,-1,-1);
-let goldfish = new Fish("Goldfish","goldfish.png",1300,"Pond",1,0,2359,-1,-1,1,12,-1,-1);
-let popEyedGoldfish = new Fish("Pop-Eyed Goldfish","popeyedgoldfish.png",1300,"Pond",1,900,1600,-1,-1,1,12);
-let rachuGoldfish = new Fish("Ranchu Goldfish","ranchugoldfish.png",4500,"Pond",2,900,1600,-1,-1,1,12,-1,-1);
-let killifish = new Fish("Killifish","killifish.png",300,"Pond",1,2359,-1,-1,4,8,-1,-1);
-let crawfish = new Fish("Crawfish","crawfish.png",200,"Pond",2,0,2359,-1,-1,4,9,-1,-1);
+let bitterling = new Fish("Bitterling","bitterling.png",900,"River",1,0,2359,11,03);
+let paleChub = new Fish("Pale Chub","palechub.png",200,"River",1,0900,1600,1,12);
+let crucianCarp = new Fish("Crucian Carp","cruciancarp.png",160,"River",2,0,2359,1,12);
+let dace = new Fish("Dace","dace.png",240,"River",3,1600,900,1,12);
+let carp = new Fish("Carp,","carp.png",300,"Pond",4,0,2359,1,12);
+let koi = new Fish("Koi","koi.png",4000,"Pond",4,1600,900,1,12);
+let goldfish = new Fish("Goldfish","goldfish.png",1300,"Pond",1,0,2359,1,12);
+let popEyedGoldfish = new Fish("Pop-Eyed Goldfish","popeyedgoldfish.png",1300,"Pond",1,900,1600,1,12);
+let rachuGoldfish = new Fish("Ranchu Goldfish","ranchugoldfish.png",4500,"Pond",2,900,1600,1,12);
+let killifish = new Fish("Killifish","killifish.png",300,"Pond",1,2359,4,8);
+let crawfish = new Fish("Crawfish","crawfish.png",200,"Pond",2,0,2359,4,9);
 
 fishList.push(bitterling);
 fishList.push(paleChub);
