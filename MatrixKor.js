b9c83870b1be2ca0d33f5a25cf6cc49b6e49eb1a
Deren <dbozerprogramming@gmail.com>
2022-01-07 23:48:20 -0500

    More characters
    Doubled the katakana and hangul characters

--- MatrixKor.js
+++ MatrixKor.js
@@ -4,16 +4,16 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン'; //7 * 11 = 77
-const korean1 = 'ㅂㅈㅉㄷㄸㄱㄲㅅㅆㅁㄴㅇㄹㅎㅋㅌㅊㅍ'; // 18
-const korean2 = 'ㅛㅕㅑㅐㅔㅗㅓㅏㅣㅠㅜㅡ'; // 12
-const korean3 = '일주달년시분후일방것집차말물큰긴짱헐물밥빵집네꽤늘'; // 25
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッンアァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const korean1 = 'ㅂㅈㅉㄷㄸㄱㄲㅅㅆㅁㄴㅇㄹㅎㅋㅌㅊㅍㅂㅈㅉㄷㄸㄱㄲㅅㅆㅁㄴㅇㄹㅎㅋㅌㅊㅍㅂㅈㅉㄷㄸㄱㄲㅅㅆㅁㄴㅇㄹㅎㅋㅌㅊㅍ';
+const korean2 = 'ㅛㅕㅑㅐㅔㅗㅓㅏㅣㅠㅜㅡㅛㅕㅑㅐㅔㅗㅓㅏㅣㅠㅜㅡㅛㅕㅑㅐㅔㅗㅓㅏㅣㅠㅜㅡ';
+const korean3 = '일주달년시분후일방것집차말물큰긴짱헐물밥빵집네꽤늘일주달년시분후일방것집차말물큰긴짱헐물밥빵집네꽤늘일주달년시분후일방것집차말물큰긴짱헐물밥빵집네꽤늘';
+const korean4 = '안녕하세요주세요죄송합니다아니요아마도도와주세요저기요안녕하세요주세요죄송합니다아니요아마도도와주세요저기요안녕하세요주세요죄송합니다아니요아마도도와주세요저기요';
 const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const nums = '0123456789';
-// 55
 
 //const alphabet = katakana + latin + nums;
-const alphabet = korean1 + korean2 + korean3 + latin + nums + katakana;
+const alphabet = korean1 + korean2 + korean3 + korean4 + latin + nums + katakana;
 
 const fontSize = 16;
 const columns = canvas.width/fontSize;
@@ -44,4 +44,4 @@ const draw = () => {
 };
 
 //Sets the speed of the rain. Higher #, lower speed
-setInterval(draw, 50);
+setInterval(draw, 40);
